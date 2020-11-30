8414273bd351c993974486925bcc4019f505697d
Caroline <carolinechatelon@hotmail.fr>
2020-11-30 15:45:15 +0100

    remove data instock and computde inStock()

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ var app =new Vue({
     data:{
         brand:"Vue Mastery",
         product:'Socks',
-        image:"/assets/socks-green.jpg",
-        inStock:false,
+        selectedVariant:0,
         details:['80% cotton', '20% polyester', "Gender-neutral"],
         variants:[
             {
                 variantId: 2234,
                 variantColor:"green",
-                variantImage:"/assets/socks-green.jpg"
+                variantImage:"/assets/socks-green.jpg",
+                variantQuantity:10,
             },
             {
                 variantId: 2235,
                 variantColor:"blue",
-                variantImage:"/assets/socks-blue.png"
+                variantImage:"/assets/socks-blue.png",
+                variantQuantity:0,
             }
         ],
         cart:0,
@@ -30,13 +31,20 @@ var app =new Vue({
         addToCart: function (){
             this.cart +=1;
         },
-        updateProduct( variantImage){
-            this.image = variantImage;
+        updateProduct( index ){
+            this.selectedVariant = index;
+            console.log(index)
         }
     },
     computed:{
         title(){
             return this.brand + ' ' + this.product;
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
